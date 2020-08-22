84b607638955bdcd90b0a79e9964b96edcc25d78
Daniel Gipson <dannygipson95@gmail.com>
2020-08-22 11:28:52 -0400

    created initial app in intellij

---
+++ src/test/java/com/dgbuild/watermyplants/WatermyplantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgbuild.watermyplants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatermyplantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
